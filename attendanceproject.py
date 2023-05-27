df6cfc638128f81abe256b4109ab90a2c7fb648c
ganesh umakant sonwale <107629096+Ganu1811@users.noreply.github.com>
2023-05-27 18:08:34 +0530

    smart_attendance_system.py

--- attendanceproject.py
+++ attendanceproject.py
@@ -1,6 +1,30 @@
-
 import cv2
 import numpy as np
+import face_recognition as fr
 import os
-import face_recognition
+from datetime import datetime
+import pyttsx3
+
+path = 'images'
+image = []
+student_name = []
+mylist = os.listdir(path)
+
+#storing names of students
+for i in mylist:
+    current_image = cv2.imread(f"{path}\{i}")
+    image.append(current_image)
+    student_name.append(os.path.splitext(i)[0])
+    
+# finding encoding of samples
+def findencoding(image):
+    encoding_list = []
+    for img in image:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encoding = fr.face_encodings(img)[0]
+        encoding_list.append(encoding)
+    return encoding_list
+  
+# finding encoding of stored samples
+encode_list = findencoding(image)
 
