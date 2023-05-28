905a617c7228004d2c376a019df56db612594d91
ganesh umakant sonwale <107629096+Ganu1811@users.noreply.github.com>
2023-05-28 20:01:18 +0530

    attendanceproject.py

--- attendanceproject.py
+++ attendanceproject.py
@@ -3,7 +3,6 @@ import numpy as np
 import face_recognition as fr
 import os
 from datetime import datetime
-import pyttsx3
 
 path = 'images'
 image = []
@@ -27,4 +26,45 @@ def findencoding(image):
   
 # finding encoding of stored samples
 encode_list = findencoding(image)
+
+def markattendance(name):
+    with open('attendance.csv', 'r+') as f:
+        mydatalist = f.readline()
+        namelist = []
+        for line in mydatalist:
+            entry = line.split(',')
+            namelist.append(entry[0])
+
+        if name not in namelist:
+            now = datetime.now()
+            time = now.strftime('%H:%M')
+            f.writelines(f'\n{name},{time}')
+            
+
+vid = cv2.VideoCapture(0)
+
+# finding encoding of cam video frames
+while True:
+    success, frames = vid.read()
+    #frames = cv2.cvtColor(frames, cv2.COLOR_BGR2RGB)
+    faces_in_frames = fr.face_locations(frames)
+    encode_in_frames = fr.face_encodings(frames, faces_in_frames)
+
+    for encode_frame, faceloc in zip(encode_in_frames, faces_in_frames):
+        match = fr.compare_faces(encode_list, encode_frame)
+        facedis = fr.face_distance(encode_list, encode_frame)
+        print(facedis)
+        matchindex = np.argmin(facedis)
+
+        if match[matchindex]:
+            name = student_name[matchindex].upper()
+            y1, x2, y2, x1 = faceloc
+            y1, x2, y2, x1 = y1+4, x2+4, y2+4, x1+4
+            cv2.rectangle(frames, (x1, y1), (x2, y2), (0,255,0), 3)
+            cv2.rectangle(frames, (x1, y2-25), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(frames, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            markattendance(name)
+            
+    cv2.imshow('Video', frames)
+    cv2.waitKey(1)
 
